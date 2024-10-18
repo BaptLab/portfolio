4de0ca164c9bfd353a26b4032a5cd48974d616ec
BaptLab <baptiste.labaune@gmail.com>
2024-10-18 11:14:55 +0200

    work on font and seo

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,20 @@ import {
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Add structured data for the website logo */}
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "Organization",
+              url: "https://www.baptistelabaune.com",
+              logo: "https://www.baptistelabaune.com/logo.png",
+            }),
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home({ offres, projects }) {
           name="viewport"
           content="width=device-width, initial-scale=1"
         />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/logo.png" />
       </Head>
       <Header />
       <main>
